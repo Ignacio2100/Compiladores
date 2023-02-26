d3857399605fa26a56ba3df1b1b8cd3ad8b7fa25
Manuel Escobar <ignaciodeleone8@gmail.com>
2023-02-25 20:56:23 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace AnalisisLexico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Pizarra = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvtabladatos = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvtabladatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.groupBox1.Controls.Add(this.PictureBox1);
+            this.groupBox1.Controls.Add(this.Pizarra);
+            this.groupBox1.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(11, 11);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(694, 279);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Introduce Codigo:";
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Location = new System.Drawing.Point(12, 23);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+            this.PictureBox1.Size = new System.Drawing.Size(30, 238);
+            this.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.PictureBox1.TabIndex = 1;
+            this.PictureBox1.TabStop = false;
+            this.PictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
+            // 
+            // Pizarra
+            // 
+            this.Pizarra.Location = new System.Drawing.Point(41, 23);
+            this.Pizarra.Margin = new System.Windows.Forms.Padding(2);
+            this.Pizarra.Name = "Pizarra";
+            this.Pizarra.Size = new System.Drawing.Size(640, 238);
+            this.Pizarra.TabIndex = 0;
+            this.Pizarra.Text = "";
+            this.Pizarra.DockChanged += new System.EventHandler(this.Fill);
+            this.Pizarra.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.groupBox2.Controls.Add(this.dgvtabladatos);
+            this.groupBox2.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 358);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(695, 354);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Distribucion";
+            // 
+            // dgvtabladatos
+            // 
+            this.dgvtabladatos.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dgvtabladatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvtabladatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dgvtabladatos.Location = new System.Drawing.Point(11, 22);
+            this.dgvtabladatos.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvtabladatos.Name = "dgvtabladatos";
+            this.dgvtabladatos.RowHeadersWidth = 51;
+            this.dgvtabladatos.RowTemplate.Height = 24;
+            this.dgvtabladatos.Size = new System.Drawing.Size(671, 313);
+            this.dgvtabladatos.TabIndex = 1;
+            this.dgvtabladatos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvInfo_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Componente léxico";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Palabra Ingresada";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Columna";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Fila";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnProcesar.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcesar.Location = new System.Drawing.Point(21, 308);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(107, 35);
+            this.btnProcesar.TabIndex = 3;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(714, 716);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Compilador UMG";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvtabladatos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox Pizarra;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvtabladatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.PictureBox PictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace AnalisisLexico
+{
+   
+    public partial class Form1 : Form
+    {
+       // int enumeracion = 0;
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 10; // se va actualizando el picturebox
+            timer1.Start();
+        }
+
+        public string Descomponer(string palabra)
+        {
+            string caracterRerpresentado = "";
+            switch (palabra) // aqui agregamos cualquier palabra
+            {
+                case "comenzar":
+                    caracterRerpresentado = "Palabra reservada de inicio";
+                    break;
+                case "cadena":
+                    caracterRerpresentado = "Variable de tipo string";
+                    break;
+                case "entero":
+                    caracterRerpresentado = "Variable de tipo int";
+                    break;
+                case "decimal":
+                    caracterRerpresentado = "Variable de tipo decimal";
+                    break;
+                case ";":
+                    caracterRerpresentado = "Signo de cierre de sentencia";
+                    break;
+                case "=":
+                    caracterRerpresentado = "Signo de asignación";
+                    break;
+                case "==":
+                    caracterRerpresentado = "Signo de comparación";
+                    break;
+                case "+":
+                    caracterRerpresentado = "Signo de suma";
+                    break;
+                case "-":
+                    caracterRerpresentado = "Signo de resta";
+                    break;
+                case ">":
+                    caracterRerpresentado = "Signo mayor";
+                    break;
+                case "<":
+                    caracterRerpresentado = "Signo menor";
+                    break;
+                case "=<":
+                    caracterRerpresentado = "Menor igual que";
+                    break;
+                case "=>":
+                    caracterRerpresentado = "Mayor igual que";
+                    break;
+                case "(":
+                    caracterRerpresentado = "Parentesis izquierdo";
+                    break;
+                case ")":
+                    caracterRerpresentado = "Parentesis derecho";
+                    break;
+                case "{":
+                    caracterRerpresentado = "Llave derecho";
+                    break;
+                case "}":
+                    caracterRerpresentado = "Llave izquierdo";
+                    break;
+                case "[":
+                    caracterRerpresentado = "Corchete izquierdo";
+                    break;
+                case "]":
+                    caracterRerpresentado = "Corchete derecho";
+                    break;
+                case "&":
+                    caracterRerpresentado = "Operador AND";
+                    break;
+                case "||":
+                    caracterRerpresentado = "Operador OR";
+                    break;
+                case "fin":
+                    caracterRerpresentado = "Palabra reservada de Final";
+                    break;
+                case "ifi":
+                    caracterRerpresentado = "Condicion If";
+                    break;
+                case "fore":
+                    caracterRerpresentado = "Ciclo For";
+                    break;
+                default:
+                    int val = 0;
+                    float decimall ;
+                    int validacion = 0;
+                        //validamos si es un numero o no
+                    if (int.TryParse(palabra, out val))
+                    {
+                        caracterRerpresentado = "Es un número";
+                        validacion = 1;
+                    }else
+                    //validamos si es un numero decimal o no
+                    if (float.TryParse(palabra, out decimall))
+                    {
+                        caracterRerpresentado = "Es un número decimal";
+                        validacion = 1;
+                    }
+                    //validamos si es una variable o no
+                    if (palabra.StartsWith("'") && palabra.EndsWith("'"))
+                    {
+                        caracterRerpresentado = "Es un nombre de variable";
+                        validacion = 1;
+                    }
+                    //validamos si es identificador o no
+                    if (validacion == 0)
+                    {
+                        caracterRerpresentado = "Nombre de Variable";
+                    }
+                    break;
+            }
+            return caracterRerpresentado;
+        }
+        public void Analisis()
+        {
+            string codigo = Pizarra.Text; // variable que identifica al cuadro blanco
+
+            char[] jump = { '\n' }; // salto de linea
+
+            char[] delimitador = { ' ' };
+
+            string[] lineas = codigo.Split(jump); // se pasa a una arrays
+
+            for (int i = 0; i < lineas.Length; i++)
+            {
+                string[] words = lineas[i].Split(delimitador);
+
+                for (int z = 0; z < words.Length; z++)
+                {
+                    words[z] = words[z].Replace("\n", "");
+
+                    if (words[z] != "")
+                    {
+                        DataGridViewRow fila = (DataGridViewRow)dgvtabladatos.Rows[0].Clone();
+                        fila.Cells[0].Value = Descomponer(words[z]);
+                        fila.Cells[1].Value = words[z];
+                        fila.Cells[2].Value = z + 1;
+                        fila.Cells[3].Value = i + 1;
+                        dgvtabladatos.Rows.Add(fila); 
+                    }
+                }
+            }
+
+        }
+
+
+       public void borrar()
+        {
+            this.dgvtabladatos.Rows.Clear(); 
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            // borrar();// si quitas los comentarios explota
+            // Analisis();
+            //personalizado(); //pruebas
+
+           
+
+        }
+
+        private void Form1_Load(System.Object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dgvInfo_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            borrar();
+            Analisis();
+            personalizado(); 
+
+        }
+
+        // Establece el color de la palabra "ejemplo" en el control RichTextBox1
+        public void personalizado()
+        {
+            // Busca la palabra "comenzar" en el control RichTextBox1
+            int uno = Pizarra.Find("comenzar");
+
+            // Si se encuentra la palabra, establece su color
+            if (uno != -1)
+            {
+                Pizarra.Select(uno, "comenzar".Length);
+                Pizarra.SelectionColor = Color.Red;
+            }
+
+            // Busca la palabra "entero" en el control RichTextBox1
+            int dos = Pizarra.Find("entero");
+
+            // Si se encuentra la palabra, establece su color
+            if (dos != -1)
+            {
+                Pizarra.Select(dos, "entero".Length);
+                Pizarra.SelectionColor = Color.Red;
+            }
+
+            // Busca la palabra "fin" en el control RichTextBox1
+            int tres = Pizarra.Find("fin");
+
+            // Si se encuentra la palabra, establece su color
+            if (tres != -1)
+            {
+                Pizarra.Select(tres, "fin".Length);
+                Pizarra.SelectionColor = Color.Red;
+            }
+
+            // Busca la palabra "cadena" en el control RichTextBox1
+            int cuatro = Pizarra.Find("cadena");
+
+            // Si se encuentra la palabra, establece su color
+            if (cuatro != -1)
+            {
+                Pizarra.Select(cuatro, "cadena".Length);
+                Pizarra.SelectionColor = Color.Red;
+            }
+
+            // Busca la palabra "cadena" en el control RichTextBox1
+            int cinco = Pizarra.Find("decimal");
+
+            // Si se encuentra la palabra, establece su color
+            if (cinco != -1)
+            {
+                Pizarra.Select(cinco, "decimal".Length);
+                Pizarra.SelectionColor = Color.Red;
+            }
+
+            // Busca la palabra "ifi" en el control RichTextBox1
+            int seis = Pizarra.Find("ifi");
+
+            // Si se encuentra la palabra, establece su color
+            if (seis != -1)
+            {
+                Pizarra.Select(seis, "ifi".Length);
+                Pizarra.SelectionColor = Color.Red;
+            }
+
+            // Busca la palabra "fore" en el control RichTextBox1
+            int siete = Pizarra.Find("fore");
+
+            // Si se encuentra la palabra, establece su color
+            if (siete != -1)
+            {
+                Pizarra.Select(siete, "fore".Length);
+                Pizarra.SelectionColor = Color.Red;
+            }
+
+           
+
+        }
+
+        private void Lineas1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+       
+            
+        }
+
+        private void timer1_Tick(System.Object sender, System.EventArgs e)
+        {
+            
+                PictureBox1.Refresh(); // se actualiza el picturebox  
+            
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            //enumeracion = 0; // SE INICIALIZA A 0 EN CADA REPINTADO
+
+            //int ALTURA = Pizarra.GetPositionFromCharIndex(0).Y; // COORDENADA Y DEL PRIMER CARACTER
+
+            //if (Pizarra.Lines.Length > 0)
+            //{
+            //    for (int I = 0; I <= Pizarra.Lines.Length - 1; I++)
+            //    {
+            //        e.Graphics.DrawString((I + 1).ToString(), Pizarra.Font, Brushes.Blue, Pizarra.Width - (Convert.ToInt32(e.Graphics.MeasureString((I + 1).ToString(), Pizarra.Font).Width) + 10), ALTURA);
+            //        enumeracion += Pizarra.Lines.Length + 1; // INDICE DEL PRIMER CARACTER DE LA LINEA SIGUIENTE
+            //        ALTURA = Pizarra.GetPositionFromCharIndex(enumeracion).Y; // POSICION EN Y DEL PRIMER CARACTER DE LA LINEA SIGUIENTE
+            //    }
+            //}
+            //else
+            //    e.Graphics.DrawString("2", Pizarra.Font, Brushes.Blue, PictureBox1.Width - Convert.ToInt32(e.Graphics.MeasureString("1", Pizarra.Font).Width) - 10, ALTURA);
+
+
+
+            // Obtener la posición del primer carácter del control RichTextBox
+            int index = Pizarra.GetCharIndexFromPosition(new Point(0, 0));
+            int firstLine = Pizarra.GetLineFromCharIndex(index);
+        
+
+            // Obtener la altura de cada línea del control RichTextBox
+            double lineHeight = Pizarra.Font.Height * 1.25;
+
+            // Obtener el número de líneas del control RichTextBox
+            int lineCount = Pizarra.Lines.Length;
+
+            // Dibujar el número de línea para cada línea del control RichTextBox
+            for (int i = 0; i < lineCount; i++)
+            {
+                // Obtener la posición del primer carácter de la línea actual
+                int lineStart = Pizarra.GetFirstCharIndexFromLine(i);
+
+                // Obtener el número de línea actual
+                double lineNumber = i + 1;
+
+                // Dibujar el número de línea en el PictureBox
+                e.Graphics.DrawString(lineNumber.ToString(), Pizarra.Font, Brushes.Black, PictureBox1.Width - e.Graphics.MeasureString(lineNumber.ToString(), Pizarra.Font).Width - 10, Convert.ToInt32(lineHeight) * i);
+
+                // Añadir el ancho de la línea actual al índice para obtener el índice de la primera línea siguiente
+                index += Pizarra.Lines[i].Length + 1;
+            }
+
+        }
+
+        private void Fill(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+    
+
